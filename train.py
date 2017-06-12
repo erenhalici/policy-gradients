329b74132eb82e62c5b24b409b98e138fe923f17
Eren Halici <eren@halici.com.tr>
2017-06-13 00:57:48 +0300

    branch for pong

--- train.py
+++ train.py
@@ -1,25 +1,23 @@
 
-import sys, os.path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'toy_env'))
-
 import numpy as np
 import gym
 import random
 from gym import wrappers
 from model import *
-import toy_env
+
+import time
+from PIL import Image
 
 random.seed()
 
 episode_count = 1000000
 steps_per_episode = 1000
 step_size = 0
-temporal_window = 1
-epsilon_steps = 1
-min_epsilon = 0.0
+epsilon_steps = 100000
+min_epsilon = 0.05
 replay_size = 1
 batch_size = 1
-fc_sizes = [128]
+fc_sizes = [128, 128]
 learning_rate = 1e-3
 reward_gamma = 0.99
 render = True
@@ -28,54 +26,43 @@ restore = False
 
 episode_count = 1000000
 steps_per_episode = 10000
-temporal_window = 4
-replay_size = 100
-batch_size = 100
+step_size = 0
+epsilon_steps = 1
+min_epsilon = 0.0
+replay_size = 10
+batch_size = 10
 fc_sizes = [128]
-learning_rate = 1e-4
+learning_rate = 1e-3
 reward_gamma = 0.99
+env_name = 'Pong-v0'
 max_action = False
-env_name = 'LunarLander-v2'
 
 # episode_count = 1000000
-# steps_per_episode = 1000
+# steps_per_episode = 10000
 # step_size = 0
-# # epsilon_steps = 100000
 # epsilon_steps = 1
-# # min_epsilon = 0.05
 # min_epsilon = 0.0
 # replay_size = 10
 # batch_size = 10
-# fc_sizes = [128, 128]
+# fc_sizes = [128]
 # learning_rate = 1e-3
 # reward_gamma = 0.99
-# env_name = 'CartPole-v1'
+# env_name = 'Breakout-v0'
+# max_action = False
 
-# episode_count = 10000
-# steps_per_episode = 1000
-# step_size = 0
-# # epsilon_steps = 1000
-# epsilon_steps = 10
-# min_epsilon = 0.00001
-# replay_size = 1
-# batch_size = 1
-# fc_sizes = [128, 128]
-# learning_rate = 1e-5
-# reward_gamma = 0.99
-# env_name = 'Toy-v0'
-
-from gym.envs.registration import registry, register, make, spec
-register(
-  id='Toy-v0',
-  entry_point='toy_env:Toy',
-  timestep_limit=3,
-  nondeterministic=False,
-)
+def prepro(I):
+  """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+  I = I[35:195] # crop
+  I = I[::2,::2,0] # downsample by factor of 2
+  I[I == 144] = 0 # erase background (background type 1)
+  I[I == 109] = 0 # erase background (background type 2)
+  I[I != 0] = 1 # everything else (paddles, ball) just set to 1
+  return I.astype(np.float).ravel()
 
 env = gym.make(env_name)
-num_inputs  = env.observation_space.shape[0] * temporal_window
-# num_inputs  = env.observation_space.n
-num_outputs = env.action_space.n
+num_inputs  = 80*80
+# num_outputs = env.action_space.n
+num_outputs = 2
 
 model = Model(num_inputs, num_outputs, fc_sizes=fc_sizes, learning_rate=learning_rate)
 
@@ -94,18 +81,13 @@ def train_model():
   if len(experience_replay) >= batch_size:
     model.train(random.sample(experience_replay, batch_size), expected_reward, reward_std)
 
-expected_reward = 0
+expected_reward = None
 reward_std = 0
 
-def get_state():
-  return np.stack(temporal_queue).ravel()
+prev_state = None
 
 for i in range(episode_count):
-  temporal_queue = []
   state = env.reset()
-  for j in range(temporal_window):
-    temporal_queue.append(state)
-
   total_reward = 0
 
   experience = []
@@ -113,25 +95,31 @@ for i in range(episode_count):
   for t in range(steps_per_episode):
     if render: env.render()
 
+    cur_state = prepro(state)
+    state = cur_state - prev_state if prev_state is not None else np.zeros(num_inputs)
+    # state = cur_state
+    # Image.fromarray(state.reshape(80,80)*255).show()
+    # time.sleep(1)
+    prev_state = cur_state
+
     if random.random() < epsilon:
-      action = env.action_space.sample()
+      # action = env.action_space.sample()
+      action = random.randint(0,1)
+      # print action
     else:
-      if max_action: action = model.get_max_action(get_state())
-      else: action = model.get_action(get_state())
+      if max_action: action = model.get_max_action(state)
+      else: action = model.get_action(state)
       # print "max action is: " + str(action)
 
     if epsilon > min_epsilon:
       epsilon -= (1.0 - min_epsilon)/epsilon_steps + 0.00000000001
 
     # print "Taking action: {}".format(action)
-    new_state, reward, done, info = env.step(action)
+    new_state, reward, done, info = env.step(action+2)
     # if done and t >= 599:
     #   reward += 200
-    temporal_queue.pop(0)
-    temporal_queue.append(new_state)
 
-
-    experience.append((get_state(), action, reward))
+    experience.append((state, action, reward))
     state = new_state
     total_reward += reward
 
@@ -143,19 +131,20 @@ for i in range(episode_count):
       experience = []
 
     if done:
+      if expected_reward == None: expected_reward = total_reward
       expected_reward = reward_gamma * expected_reward + (1 - reward_gamma) * total_reward
       reward_std = reward_gamma * reward_std + (1 - reward_gamma) * abs(expected_reward - total_reward)
       add_experience(experience)
-      if (i % batch_size) == 0:
+      if i%batch_size == 0:
         train_model()
         with open("output.txt", "a") as outfile:
-          string = ("Episode {0:05d} finished after {1:03d} timesteps. Total Reward: {2:03.2f} Expected Reward: {3:03.2f} Reward Std: {4:03.2f} (epsilon: {5:.2f})".format(i, t+1, total_reward, expected_reward, reward_std, epsilon))
-          outfile.write(string + '\n')
+          string = "Episode {0:05d} finished after {1:03d} timesteps. Total Reward: {2:03.2f} Expected Reward: {3:03.2f} Reward Std: {4:03.2f} (epsilon: {5:.2f})".format(i, t+1, total_reward, expected_reward, reward_std, epsilon)
+          outfile.write(string + "\n")
           print(string)
 
-      if (i % 1000) == 0:
+      if (i % 100) == 0:
         model.save()
 
       break
 
-  # print("Episode {0:05d} finished after {1:03d} timesteps. Total Reward: {2:03.2f} Expected Reward: {3:03.2f} Reward Std: {4:03.2f} (epsilon: {5:.2f})".format(i, t+1, total_reward, expected_reward, reward_std, epsilon))
+
